c39b8359d44dc85afa865eeb06906bc36f46bf41
Dmitriy Vidonov <dmitriy.vidonov@gmail.com>
2023-04-06 00:09:37 +0300

    method AddKey() is done. Method FindKeyIndex() is partly done. Not all tests are passed

--- src/test/java/ex_3/test_version/Various_Methods_Test.java
+++ src/test/java/ex_3/test_version/Various_Methods_Test.java
@@ -3,6 +3,11 @@ package ex_3.test_version;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class Various_Methods_Test {
 
     int depth = 3;
@@ -27,9 +32,38 @@ class Various_Methods_Test {
         treeTest.AddKey(25);
         treeTest.AddKey(200);
         treeTest.AddKey(75);
-        System.out.println("индекс 125 = " + treeTest.AddKey(125));
+        //System.out.println("индекс 125 = " + treeTest.AddKey(125));
         System.out.println("-1 если не удалось " + treeTest.AddKey(250));
         // Выведем снова
         treeTest.ShowArray();
+
+        System.out.println("Результат FindKey = " + treeTest.FindKeyIndex(200));
+    }
+
+    @Test
+    @DisplayName("3) Проверка добавления и поиска")
+    void testAddKeyAndFindKeyIndex() throws Exception{
+        int depth = 3;
+        aBST tree = new aBST(depth);
+
+        // Проверка добавления ключей
+        assertEquals(0, tree.AddKey(50));
+        assertEquals(1, tree.AddKey(25));
+        assertEquals(2, tree.AddKey(75));
+        assertEquals(3, tree.AddKey(10));
+        assertEquals(4, tree.AddKey(40));
+        assertEquals(6, tree.AddKey(100)); // Дерево заполнено с одним null
+
+        // Проверка поиска ключей
+        assertEquals(0, tree.FindKeyIndex(50));
+        assertEquals(1, tree.FindKeyIndex(25));
+        assertEquals(2, tree.FindKeyIndex(75));
+        assertEquals(-5, (int)tree.FindKeyIndex(60)); // Найден незаполненный слот
+       // assertNull(tree.FindKeyIndex(200)); // Ключ не найден
+        System.out.println(tree.FindKeyIndex(200));
+
+        // Проверка пустого дерева
+       // aBST emptyTree = new aBST(depth);
+       // assertNull(emptyTree.FindKeyIndex(50)); // Ключ не найден
     }
 }
